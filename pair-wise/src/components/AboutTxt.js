a033bb14bf0e063e6b86e5b7351559e23263f347
AusEspi <aespi125@ucr.edu>
2025-03-21 01:56:46 -0700

    about page message done

--- pair-wise/src/components/AboutTxt.js
+++ pair-wise/src/components/AboutTxt.js
@@ -5,11 +5,12 @@ function AboutTxt() {
   return (
     <div className="about-container">
       <h1>About Us :)</h1>
-      <h3>We are team AHA, the creators of [insert name]</h3>
-      <p>Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look:</p>
-      <p>Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure.</p>
-      <p>Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur.Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut.</p>
-      <p>Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur.</p>
+      <h3>We are team AHA, the creators of the Pair Concept Project</h3>
+      <p>Our goal as a team is to help create new insights into research articles with a streamlined experience for the average researcher.</p>
+      <p>Being stidents at the Univercity of California in Riverside we have worked together with professor Benjamin Stewart and his idea of analyzing pair concepts within articles to create this website. Our future goal is to analyze the changes in searched terms over time, allowing users to filter through and explore various changes in words over the courses of their lives.</p>
+      <p>Overall, this tool is open to any who wish to utilize it, whether they use it for their own research or simply out of curiosity. We simply aim to create a strealined experience for researchers who wish to analyze or utilize the changes in pair terms.</p>
+      <p>From our team to you, we hope you enjoy your time spent here! :)</p>
+      <p>-Team AHA</p>
     </div>
   );
 }
